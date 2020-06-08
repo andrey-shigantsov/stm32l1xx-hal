278f3160128fa6ca6be1aa0c1bbaa64e8bb2512e
Andrey Shigantsov <shigantsov.a.a@gmail.com>
2020-06-08 23:53:36 +0600

    SerialExt prelude added

--- src/prelude.rs
+++ src/prelude.rs
@@ -12,6 +12,7 @@ pub use crate::delay::DelayExt as _stm32l1xx_hal_delay_DelayExt;
 pub use crate::dma::DmaExt as _stm32l1xx_hal_dma_DmaExt;
 pub use crate::exti::ExtiExt as _stm32l1xx_hal_exti_ExtiExt;
 pub use crate::gpio::GpioExt as _stm32l1xx_hal_gpio_GpioExt;
+pub use crate::serial::SerialExt as _stm32l1xx_hal_serial_SerialExt;
 pub use crate::i2c::I2cExt as _stm32l1xx_hal_i2c_I2Ext;
 pub use crate::pwm::PwmExt as _stm32l1xx_hal_pwm_PwmExt;
 pub use crate::qei::QeiExt as _stm32l1xx_hal_qei_QeiExt;
